4deede437d1bb95059b4947f1fc41594205d0c37
zearkiatos <caprilespe@outlook.com>
2023-07-24 20:19:34 -0400

    Function type

---
+++ src/components/Functions/FunctionType.ts
@@ -0,0 +1,8 @@
+import { ButtonClickHandler } from "../Button/ButtonType";
+
+type FunctionType = {
+  onContentClear: ButtonClickHandler;
+  onDelete: ButtonClickHandler;
+};
+
+export default FunctionType;
--- src/components/Functions/index.tsx
+++ src/components/Functions/index.tsx
@@ -1,17 +1,12 @@
-import React from "react";
-import PropTypes from "prop-types";
+import { FC } from 'react';
 import Button from "../Button";
+import FunctionType from "./FunctionType";
 
-const Functions = ({ onContentClear, onDelete }) => (
+const Functions:FC<FunctionType> = ({ onContentClear, onDelete }) => (
   <div className="functions">
     <Button type="button-long-text" text="clear" clickHandler={onContentClear} />
     <Button text="⬅" clickHandler={onDelete} />
   </div>
 );
-
-Functions.propTypes = {
-  onContentClear: PropTypes.func.isRequired,
-  onDelete: PropTypes.func.isRequired
-};
 
 export default Functions;
